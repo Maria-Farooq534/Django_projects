97579313349b0d1d81b984985f9bfc64e0b6279c
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-07-06 00:41:51 -0700

    Adding about function

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -6,4 +6,9 @@ from django.http import HttpResponse
 def index(request):  #By default, the index function takes an argument named "request"
     # return "Hello, World" 
     #Now I need to return string in HttpResponse instead of blank string.
-    return HttpResponse("Hello, World")
+    return HttpResponse("Hello, and Welcome")
+
+def about(request):
+    # return HttpResponse("About my Website")
+    #We can use html tags in it
+    return HttpResponse("<h1>About my Website</h1>")
